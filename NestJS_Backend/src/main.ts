27cfc53eb73743b4a39787f8a335f541354366bc
Austen Anop <austinanop@gmail.com>
2024-04-13 17:59:42 -0400

    Update the main.ts

--- NestJS_Backend/src/main.ts
+++ NestJS_Backend/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AuthModule } from './Auth/auth.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, { cors: true });
-  const app = await NestFactory.create(AuthModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
 }
 bootstrap();
