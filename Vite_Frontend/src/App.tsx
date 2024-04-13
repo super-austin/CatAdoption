7e3cd62b79ddea25acc05eef007de9eee0b9afa8
Austen Anop <austinanop@gmail.com>
2024-04-13 13:33:49 -0400

    Add URL Bar

--- Vite_Frontend/src/App.tsx
+++ Vite_Frontend/src/App.tsx
@@ -1,5 +1,27 @@
-const App = () => {
-  return <>This is Vite App</>;
+import { FC, useState } from "react";
+import URLBar from "./components/URLBar/URLBar";
+import { HTTPMethodEnum } from "./common.type";
+
+const App: FC = () => {
+  const [url, setUrl] = useState("");
+  const [methods, setMethods] = useState(HTTPMethodEnum.GET);
+
+  const sendRequest = () => {
+    alert("Submit Request");
+  };
+
+  return (
+    <main className="w-full flex flex-col items-center p-10 gap-4">
+      <h1 className="underline mb-3">Cat Adoption Frontend</h1>
+      <URLBar
+        methods={methods}
+        url={url}
+        setMethods={setMethods}
+        setUrl={setUrl}
+        sendRequest={sendRequest}
+      />
+    </main>
+  );
 };
 
 export default App;
